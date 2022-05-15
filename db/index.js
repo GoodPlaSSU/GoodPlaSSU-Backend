27d3a44f0ff328e317454a1821eb77523346216c
kgh128 <hayoung0627@gmail.com>
2022-05-15 19:53:38 +0900

    Add module.exports in db/index.js

--- db/index.js
+++ db/index.js
@@ -1,14 +1,16 @@
 const { Client } = require('pg');
 require('dotenv').config();
 
-const client = new Client({
+const db = new Client({
     connectionString: process.env.DB_URI,
     ssl: {
         rejectUnauthorized: false,
     }
 });
 
-client.connect(err => {
+db.connect(err => {
     if (err) console.log('err');
     else console.log('데이터베이스 연결 성공');
 });
+
+module.exports = db;
