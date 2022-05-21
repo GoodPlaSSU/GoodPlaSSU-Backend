7d6601f9482446c7e9dcada11a943ec3d224283c
kgh128 <hayoung0627@gmail.com>
2022-05-21 22:26:31 +0900

    Change single quote to back quote and update updated_at query

--- routes/board.js
+++ routes/board.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const pg = require('../db/index');
 
 // 게시판의 한 페이지에 해당하는 게시물 조회 API
-router.get('/board', function(req,res) {
+router.get('/', function(req,res) {
     var responseData = {};
-
     const tag = req.query.tag;
-    const sql = 'select * from board order by created_at desc;';
+
+    const sql = `select * from board order by created_at desc;`;
 
     pg.query(sql, (err, rows) => {
         if (err) console.log(err.stack);
@@ -25,12 +25,17 @@ router.get('/board', function(req,res) {
 });
 
 // 요청 받은 ID의 게시물 조회 API (게시물 상세 조회 API)
-router.get('/board/:id', (req, res) => {
+// request: id (parameter values)
+router.get('/:id', (req, res) => {
     var responseData = {};
-
     const id = req.params.id;
-    const sql1 = 'update board set view_count = view_count + 1 where id = ${id};'; // 조회수 증가 쿼리
-    const sql2 = 'select user_key, content, image1, image2, image3, image4, view_count, cheer_count, updated_at from board where id = ${id};';
+
+    const sql1 = `update board 
+                set view_count = view_count + 1 
+                where id = ${id};`; // 조회수 증가 쿼리
+    const sql2 = `select user_key, content, image1, image2, image3, image4, view_count, cheer_count, updated_at 
+                from board 
+                where id = ${id};`;
 
     var query = pg.query(sql1 + sql2, (err, rows) => {
         if (err) throw err;
@@ -48,9 +53,12 @@ router.get('/board/:id', (req, res) => {
 
 
 // 게시물 생성 API
-router.post('/board', (req, res) => {
+// request: user_key, content, image1, image2, image3, image4, tag (json)
+router.post('/', (req, res) => {
     const data = req.body;
-    const sql = 'insert into board (user_key, content, image1, image2, image3, image4, tag) values ($1, $2, $3, $4, $5, $6, $7);';
+
+    const sql = `insert into board (user_key, content, image1, image2, image3, image4, tag) 
+                values ($1, $2, $3, $4, $5, $6, $7);`;
     const dbInput = [data.user_key, data.content, data.image1, data.image2, data.image3, data.image4, data.tag];
 
     var query = pg.query(sql, dbInput, (err) => {
@@ -62,10 +70,14 @@ router.post('/board', (req, res) => {
 
 
 // 게시물 수정 API
-router.post('/board/:id', (req, res) => {
+// request: content, image1, image2, image3, image4 (json)
+router.post('/:id', (req, res) => {
     const id = req.params.id;
     const data = req.body;
-    const sql = 'update board set content = $1, image1 = $2, image2 = $3, image3 = $4, image4 = $5 where id = ${id};';
+
+    const sql = `update board 
+                set content = $1, image1 = $2, image2 = $3, image3 = $4, image4 = $5, updated_at = NOW() 
+                where id = ${id};`;
     const dbInput = [data.content, data.image1, data.image2, data.image3, data.image4];
 
     var query = pg.query(sql, dbInput, (err) => {
@@ -77,9 +89,11 @@ router.post('/board/:id', (req, res) => {
 
 
 // 게시물 삭제 API
-router.delete('/board/:id', (req, res) => {
+// request: id (parameter values)
+router.delete('/:id', (req, res) => {
     const id = req.params.id;
-    const sql = 'delete from board where id = ${id};';
+
+    const sql = `delete from board where id = ${id};`;
 
     var query = pg.query(sql, (err) => {
         if (err) throw err;
