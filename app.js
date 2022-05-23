0ba9cdbdf566cda5cff23ca70d3f67026c7cf585
kgh128 <hayoung0627@gmail.com>
2022-05-23 10:34:49 +0900

    Create routes/mypage.js and mypageRouter in app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var boardRouter = require('./routes/board');
 var commentRouter = require('./routes/comment');
+var mypageRouter = require('./routes/mypage');
 
 var app = express();
 
@@ -32,6 +33,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/board', boardRouter);
 app.use('/comment', commentRouter);
+app.use('/mypage', mypageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
