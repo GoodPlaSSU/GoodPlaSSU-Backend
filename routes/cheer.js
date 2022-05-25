7077e6f8668eaca64d4e907a162c2d4a27f54800
HayoungKim20 <hayoung0627@gmail.com>
2022-05-25 11:41:16 +0900

    Add Incresing total_point and month_point query

--- routes/cheer.js
+++ routes/cheer.js
@@ -12,11 +12,14 @@ router.post('/', (req, res) => {
 
     const sql1 = `update board
                 set cheer_count = cheer_count + 1
-                where id = ${board_key};`;
-    const sql2 = `insert into cheer (user_key, board_key)
+                where id = ${board_key};`; // 좋아요 수 증가 쿼리
+    const sql2 = `update profile
+                set total_point = total_point + 1, month_point = month_point + 1
+                where id = ${user_key};`; // 작성자 선행 포인트 증가 쿼리
+    const sql3 = `insert into cheer (user_key, board_key)
                 values (${user_key}, ${board_key});`;
 
-    pg.query(sql1+sql2, (err) => {
+    pg.query(sql1+sql2+sql3, (err) => {
         if (err) throw err;
         res.sendStatus(201);
     });
@@ -32,7 +35,7 @@ router.delete('/', (req, res) => {
 
     const sql1 = `update board
                 set cheer_count = cheer_count - 1
-                where id = ${board_key} and cheer_count > 0;`;
+                where id = ${board_key} and cheer_count > 0;`; // 좋아요 수 감소 쿼리
     const sql2 = `delete from cheer
                 where user_key = ${user_key} and board_key = ${board_key};`;
 
